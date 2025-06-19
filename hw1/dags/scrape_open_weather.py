284699d0ec63fc59499f405269a5a01322de466f
Serhii Vykaliuk <serhii.v@gmail.com>
2025-06-19 17:50:36 +0300

    Update .gitignore, requirements, and start script; refactor DAG for weather data pipeline

--- hw1/dags/scrape_open_weather.py
+++ hw1/dags/scrape_open_weather.py
@@ -1,136 +1,17 @@
 import json
 import pendulum
 from airflow.decorators import dag, task_group, task
+from airflow.models.taskinstance import TaskInstance
+from airflow.models.dagrun import DagRun
 from airflow.models.variable import Variable
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import HttpOperator
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 
-@task_group()
-def preparation():
-    """
-    Check if the OpenWeather API is available and create a SQLite database and table to store the data.
-    """
-
-    create_sqlite_table = SQLExecuteQueryOperator(
-        task_id="create_sqlite_table",
-        conn_id="weather_sqlite_conn",
-        sql="""
-            CREATE TABLE IF NOT EXISTS measures
-            (
-                timestamp TIMESTAMP,
-                city TEXT,
-                temperature FLOAT,
-                humidity INT,
-                cloudiness INT,
-                wind_speed FLOAT
-            );
-        """,
-    )
-    create_sqlite_table.doc_md = """
-    Create a SQLite database and table to store the data.
-    - SQLite database: `weather.db`
-    """
-
-    check_weather_api = HttpSensor(
-        task_id="check_weather_api",
-        http_conn_id="weather_http_conn",
-        endpoint="data/3.0/onecall",
-        request_params={
-            "lon": 24.0232,
-            "lat": 49.8383,
-            "appid": Variable.get("WEATHER_API_KEY"),
-        },
-    )
-    check_weather_api.doc_md = """
-    Check if the OpenWeather API is available.
-    - API version 3.0
-    - Endpoint: `data/3.0/onecall`
-    """
-
-
-@task_group()
-def extract():
-    """
-    Extract the data from OpenWeather API for each city.
-    """
-    for city, (lat, lon) in cities.items():
-        extract = HttpOperator(
-            task_id=f"{city}",
-            http_conn_id="weather_http_conn",
-            endpoint="data/3.0/onecall/day_summary",
-            data={
-                "lon": lon,
-                "lat": lat,
-                "date": "{{ ds }}",
-                "units": "metric",
-                "appid": Variable.get("WEATHER_API_KEY"),
-            },
-            method="GET",
-            response_filter=lambda x: json.loads(x.text),
-            log_response=True,
-        )
-        extract.doc_md = f"""
-        Extract the data from OpenWeather API for {city}.
-        - API version 3.0
-        - Endpoint: `data/3.0/onecall/day_summary`
-        - City: {city}
-        """
-
-
-@task()
-def process(ti):
-    """
-    Process the data from OpenWeather API for each city.
-    """
-    output = []
-    for city in cities.keys():
-        data = ti.xcom_pull(task_ids=f"extract.{city}")
-        output.append(
-            ", ".join(
-                [
-                    f'"{str(pendulum.from_format(data["date"], "YYYY-MM-DD", tz="Europe/Kyiv"))}"',
-                    f'"{city}"',
-                    str(data["temperature"]["afternoon"]),
-                    str(data["humidity"]["afternoon"]),
-                    str(data["cloud_cover"]["afternoon"]),
-                    str(data["wind"]["max"]["speed"]),
-                ]
-            )
-        )
-    return f"({') , ('.join(output)})"
-
-
-inject_data = SQLExecuteQueryOperator(
-    task_id="inject_data",
-    conn_id="weather_sqlite_conn",
-    params={"task_ids": "process"},
-    sql="""
-    INSERT INTO measures
-    (timestamp, city, temperature, humidity, cloudiness, wind_speed)
-    VALUES
-    {{ti.xcom_pull(task_ids=params.task_ids)}};
-    """,
-)
-inject_data.doc_md = """
-Insert the data into SQLite database.
-- SQLite database: `weather.db`
-- Table: `measures`
-"""
-
-cities = {
-    "Lviv": ("49.8383", "24.0232"),
-    "Kyiv": ("50.4333", "30.5167"),
-    "Kharkiv": ("50", "36.25"),
-    "Odesa": ("46.4775", "30.7326"),
-    "Zhmerynka": ("49.037", "28.112"),
-}
-
-
 @dag(
     schedule="@daily",
-    start_date=pendulum.datetime(2025, 4, 1, tz="Europe/Kyiv"),
+    start_date=pendulum.datetime(2025, 6, 17, tz="Europe/Kyiv"),
     catchup=True,
     tags=["de-m3-pipeline"],
 )
@@ -146,11 +27,146 @@ def scrape_open_weather():
     - Store the data in SQLite database.
     """
 
-    preparation() >> extract() >> process() >> inject_data
+    @task_group()
+    def preparation():
+        """
+        Check if the OpenWeather API is available and create a SQLite database and table to store the data.
+        """
+
+        check_sqlite_cities_data = SQLExecuteQueryOperator(
+            task_id="check_sqlite_cities_data",
+            conn_id="weather_sqlite_conn",
+            sql="SELECT city, longitude, latitude FROM cities;",
+            do_xcom_push=True,
+        )
+        check_sqlite_cities_data.doc_md = """
+        Check if the cities table exists in the SQLite database and read the data.
+        - SQLite database: `weather.db`
+        - Table: `cities`
+        - Columns: `city`, `longitude`, `latitude`
+        """
+
+        create_sqlite_table = SQLExecuteQueryOperator(
+            task_id="create_sqlite_table",
+            conn_id="weather_sqlite_conn",
+            sql="""
+                CREATE TABLE IF NOT EXISTS measures
+                (
+                    timestamp TIMESTAMP,
+                    city TEXT,
+                    temperature FLOAT,
+                    humidity INT,
+                    cloudiness INT,
+                    wind_speed FLOAT
+                );
+            """,
+        )
+        create_sqlite_table.doc_md = """
+        Create a SQLite database and table to store the data.
+        - SQLite database: `weather.db`
+        """
+
+        check_weather_api = HttpSensor(
+            task_id="check_weather_api",
+            http_conn_id="weather_http_conn",
+            # endpoint="data/3.0/onecall",
+            endpoint="data/2.5/weather",
+            request_params={
+                "lon": 24.0232,
+                "lat": 49.8383,
+                "appid": Variable.get("WEATHER_API_KEY"),
+            },
+        )
+        check_weather_api.doc_md = """
+        Check if the OpenWeather API is available.
+        - API version 2.5
+        - Endpoint: `data/2.5/weather`
+        """
+
+    # @task_group()
+    # def extract(ti: TaskInstance, **kwargs):
+    #     """
+    #     Extract the data from OpenWeather API for each city.
+    #     """
+    #     rows = ti.xcom_pull(task_ids="check_sqlite_cities_data")
+    #     for row in rows:
+    #         city, (lon, lat) = row
+    #         extract = HttpOperator(
+    #             task_id=f"{city}",
+    #             http_conn_id="weather_http_conn",
+    #             endpoint="data/3.0/onecall/day_summary",
+    #             data={
+    #                 "lon": lon,
+    #                 "lat": lat,
+    #                 "date": "{{ ds }}",
+    #                 "units": "metric",
+    #                 "appid": Variable.get("WEATHER_API_KEY"),
+    #             },
+    #             method="GET",
+    #             response_filter=lambda x: json.loads(x.text),
+    #             log_response=True,
+    #         )
+    #         extract.doc_md = f"""
+    #         Extract the data from OpenWeather API for {city}.
+    #         - API version 3.0
+    #         - Endpoint: `data/3.0/onecall/day_summary`
+    #         - City: {city}
+    #         """
+
+    # @task
+    # def process(ti: TaskInstance):
+    #     """
+    #     Process the data from OpenWeather API for each city.
+    #     """
+    #     output = []
+    #     rows = ti.xcom_pull(task_ids="check_sqlite_cities_data")
+    #     for row in rows:
+    #         city, _ = row
+
+    #         # for city in cities.keys():
+    #         data = ti.xcom_pull(task_ids=f"extract.{city}")
+    #         output.append(
+    #             ", ".join(
+    #                 [
+    #                     f'"{str(pendulum.from_format(data["date"], "YYYY-MM-DD", tz="Europe/Kyiv"))}"',
+    #                     f'"{city}"',
+    #                     str(data["temperature"]["afternoon"]),
+    #                     str(data["humidity"]["afternoon"]),
+    #                     str(data["cloud_cover"]["afternoon"]),
+    #                     str(data["wind"]["max"]["speed"]),
+    #                 ]
+    #             )
+    #         )
+    #     return f"({') , ('.join(output)})"
+
+    # inject_data = SQLExecuteQueryOperator(
+    #     task_id="inject_data",
+    #     conn_id="weather_sqlite_conn",
+    #     params={"task_ids": "process"},
+    #     sql="""
+    #     INSERT INTO measures
+    #     (timestamp, city, temperature, humidity, cloudiness, wind_speed)
+    #     VALUES
+    #     {{ti.xcom_pull(task_ids=params.task_ids)}};
+    #     """,
+    # )
+    # inject_data.doc_md = """
+    # Insert the data into SQLite database.
+    # - SQLite database: `weather.db`
+    # - Table: `measures`
+    # """
+
+    # # cities = {
+    # #     "Lviv": ("49.8383", "24.0232"),
+    # #     "Kyiv": ("50.4333", "30.5167"),
+    # #     "Kharkiv": ("50", "36.25"),
+    # #     "Odesa": ("46.4775", "30.7326"),
+    # #     "Zhmerynka": ("49.037", "28.112"),
+    # # }
+
+    # preparation() >> extract() >> process() >> inject_data  # type: ignore
+    preparation()
+    # >> extract() >> process() >> inject_data  # type: ignore
 
 
 scrape_open_weather()
-
-
-if __name__ == "__main__":
-    dag.test()
--- hw1/start_airflow.sh
+++ hw1/start_airflow.sh
@@ -1,19 +1,62 @@
-# Install environment
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
+#!/bin/bash
+# You need to have installed uv (
+# https://docs.astral.sh/uv/getting-started/installation/
 
-# Create the database for weather
+
+PYTHON_VERSION=3.12
+
+
+### Install python environment
+uv venv --python ${PYTHON_VERSION} .venv
+source .venv/bin/activate
+
+
+### Create the database for weather
 sqlite3 weather.db ".databases"
 
-### Set connections
+### Create the cities table and insert some data
+sqlite3 weather.db <<SQL
+DROP TABLE IF EXISTS cities;
+
+CREATE TABLE cities (
+    city TEXT,
+    longitude REAL,
+    latitude REAL
+);
+
+INSERT INTO cities (city, longitude, latitude) VALUES ("Lviv",49.8383,24.0232);
+INSERT INTO cities (city, longitude, latitude) VALUES ("Kyiv",50.4333,30.5167);
+INSERT INTO cities (city, longitude, latitude) VALUES ("Kharkiv",50,36.25);
+INSERT INTO cities (city, longitude, latitude) VALUES ("Odesa",46.4775,30.7326);
+INSERT INTO cities (city, longitude, latitude) VALUES ("Zhmerynka",49.037,28.112);
+SQL
+
+
+### Set airflow variables
+read -p "Please enter api key for OpenWeatherMap: " api_key
+export AIRFLOW_VAR_WEATHER_API_KEY=$api_key
+
+
+### Set airflow connections
 export AIRFLOW_CONN_WEATHER_HTTP_CONN='{"conn_type": "http", "host": "https://api.openweathermap.org"}'
 export AIRFLOW_CONN_WEATHER_SQLITE_CONN='{"conn_type": "sqlite", "host": "'$PWD'/weather.db"}'
 
-### Start Airflow
+
+### Set airflow configurations
 export AIRFLOW_HOME=$PWD/airflow
 export AIRFLOW__CORE__DAGS_FOLDER=$PWD/dags
 export AIRFLOW__CORE__LOAD_EXAMPLES=False
 export AIRFLOW__CORE__DEFAULT_TIMEZONE="Europe/Kyiv"
 
+
+### Install Airflow
+AIRFLOW_VERSION=3.0.2
+
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+# For example this would install 3.0.0 with python 3.9: https://raw.githubusercontent.com/apache/airflow/constraints-3.0.2/constraints-3.9.txt
+
+uv pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+uv pip install -r requirements.txt
+
+### Start Airflow
 airflow standalone
